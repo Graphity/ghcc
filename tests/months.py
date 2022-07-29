028aa8b76bbe601c0e90e5599afeed885a591ec3
Graphity <rohood3@gmail.com>
2022-07-29 23:01:41 +0400

    Update test for months

--- tests/months.py
+++ tests/months.py
@@ -1,4 +1,10 @@
 from ghcc import GHCC
 
 user = GHCC('Graphity')
-print(user.months)
+months = user.months
+
+for month in months:
+    print(month['name'])
+    for day in month['days']:
+        print(day)
+    print()
